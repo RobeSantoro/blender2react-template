a426db0c4dbd4ddeeeb48513ebd6dfe873fcfaa2
RobeSantoro <santoro.robe@gmail.com>
2023-07-26 15:43:22 +0200

    Setup Basic App

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Perf } from 'r3f-perf'
+
+import World from './components/World'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Canvas
+        eventPrefix="offset" 
+        frameloop="always"
+        dpr={[1, 2]}
+        gl={{ alpha: true, antialias: true, preserveDrawingBuffer: true }}
+        camera={{ fov: 100, position: [0, 0, -5], near: 1, far: 1000 }}>
+        <Suspense fallback={null}>
+          <World />
+        </Suspense>
+        <Perf position="bottom-right" />
+      </Canvas>
     </>
   )
 }
